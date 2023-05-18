adfadb7022aa61e261bc92304d6c0bc687e2ddd4
Zack Zellto <zacharyzellto@gmail.com>
2023-05-18 12:41:02 -0600

    tested locally POST worked to DB

--- src/api/waitlistAPI.ts
+++ src/api/waitlistAPI.ts
@@ -6,7 +6,7 @@ interface WaitlistResponse {
   timestamp: string;
 }
 
-const baseURL = "https://fmly-landing-backend.herokuapp.com/api/waitlist";
+const baseURL = "http://127.0.0.1:5000/api/waitlist";
 
 export const addToWaitlist = async (
   email: string
